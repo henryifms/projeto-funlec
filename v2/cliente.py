f7e27aaf249dd4ff9ab09f8b1330643f458b2946
Henry <henrytoliveriacampos@gmail.com>
2026-03-03 10:17:20 -0400

    feat: Simplyfing project

--- v2/cliente.py
+++ v2/cliente.py
@@ -1,27 +1,14 @@
 import socket
 import ctypes
-import jwt
 
-SECRET = "minha_secreta"
 UDP_PORT = 9999
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("0.0.0.0", UDP_PORT))
 
 while True:
-    data, addr = sock.recvfrom(4096)
-    
-    try:
-        payload = jwt.decode(
-            data.decode(),
-            SECRET,
-            algorithms=["HS256"]
-        )
+    data, addr = sock.recvfrom(1024)
+    msg = data.decode().strip()
 
-        if payload["cmd"] == "lock":
-            ctypes.windll.user32.LockWorkStation()
-
-    except jwt.ExpiredSignatureError:
-        pass
-    except jwt.InvalidTokenError:
-        pass  
+    if msg == "LOCK_NOW":
+        ctypes.windll.user32.LockWorkStation()
